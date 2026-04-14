3d0c77a6862e1c60b2a8f7e548ba2eaefc4840fe
Anish Kumar <ay6704197@gmail.com>
2026-04-14 10:25:16 +0530

    update inheritance 1.cpp

---
+++ inheritance 1.cpp
@@ -0,0 +1,40 @@
+class Person {
+    private : 
+   char name[20];
+   int age;
+
+   protected:
+   void setters()
+     {
+         
+     }
+    int  getters()
+     {
+         
+     }
+
+}
+
+class person :: public Employee
+  {
+    
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Define a class Person with name and age as instance variables as private members. Provide setters and getters as protected methods. Derive a class Employee from Person class with private instance variable salary. Provide public methods setEmployee() and showEmployee().*/
+
